1a3f0daa0bcefb45bd1fba1abcb261bfc162dd38
surachet033 <43290872+surachet033@users.noreply.github.com>
2018-09-16 23:31:50 +0700

    Add files via upload

---
+++ Weather/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head><title>Weather</title>
+</head>
+<body>
+	<center><br><br>
+		<form method="GET" action="get.php">
+		<h1>Type City and Country</h1>
+		<br><p>For Example Bangkok</p>
+			<input type="text" name="q" required>
+			<input type="submit" name="submit">
+		</form>
+	</center>
+</body>
+</html>
